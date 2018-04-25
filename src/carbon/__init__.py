f821b36c1d9b0c5f3e206748151e2f888669ed80
Ryan Williams <rywillia@redhat.com>
2018-04-25 13:32:24 -0400

    bump version to 0.5.0

--- src/carbon/__init__.py
+++ src/carbon/__init__.py
@@ -27,5 +27,5 @@ from .carbon import Carbon
 from .helpers import CustomDict
 from .resources import Scenario, Host, Action, Execute, Report
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __author__ = 'PIT Team Red Hat'
