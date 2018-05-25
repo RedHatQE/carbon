7f9ce15515e57f4e4cd05b8f5ceeae16191ad6d7
Ryan Williams <rywillia@redhat.com>
2018-05-25 09:30:16 -0400

    chore: rework the existing jenkins job for code reviews
    This commit renames/updates the jenkins job yaml to support
    the latest changes that were made from the user inteface. It
    includes the pipeline file which the job loads the pipeline file
    to run. Instead of directly editing the pipeline code from within
    the job.

---
+++ jenkins/JenkinsfileGerrit.groovy
@@ -0,0 +1,56 @@
+pipeline {
+    agent {
+        node {
+            label 'carbon'
+        }
+    }
+    stages {
+        stage('Clone') {
+            steps {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: "${GERRIT_PATCHSET_REVISION}"]],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [[$class: 'CleanBeforeCheckout']],
+                        submoduleCfg: [],
+                        userRemoteConfigs: [[
+                                                    credentialsId: '5ed678e3-0a20-46da-979a-4192e7523c33',
+                                                    refspec: "${GERRIT_REFSPEC}",
+                                                    url: 'ssh://qe-pit-jenkins@code.engineering.redhat.com:22/carbon'
+                                            ]]
+                ])
+            }
+        }
+        stage('Env Setup') {
+            steps {
+                sh 'virtualenv venv'
+                sh 'source venv/bin/activate'
+                sh 'pip install -r test-requirements.txt'
+            }
+        }
+        stage('Validation') {
+            parallel {
+                stage('Tox Python 2') {
+                    steps {
+                        dir('carbon') {
+                            sh 'tox -e py27-functional'
+                        }
+                    }
+                }
+                stage('Tox Python 3') {
+                    steps {
+                        dir('carbon') {
+                            sh 'tox -e py36-functional'
+                        }
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'tests/cover/**'
+            junit 'tests/cover/functional/py27/nosetests.xml, tests/cover/functional/py36/nosetests.xml'
+        }
+    }
+}
