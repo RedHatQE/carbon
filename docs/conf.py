a2f78b233c8ff163bb934b8628b451d363bd2127
Ryan Williams <rywillia@redhat.com>
2018-07-27 15:51:46 -0400

    Bump version: 0.6.0 → 0.7.0
    Change-Id: I7ebe985fbdf48d91e5e1358a6af38a1abcfab61c

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2018, Red Hat Inc.'
 author = u'Red Hat Inc.'
 
 # The short X.Y version
-version = u'0.6.0'
+version = u'0.7.0'
 # The full version, including alpha/beta/rc tags
-release = u'0.6.0'
+release = u'0.7.0'
 
 
 # -- General configuration ---------------------------------------------------
