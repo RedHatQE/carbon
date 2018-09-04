8bf47257b5dd5973201619ab92adb5cd039f8e8d
Ryan Williams <rywillia@redhat.com>
2018-09-04 13:30:52 -0400

    reduce the size of the run method for the runner executor
    This commit breaks down the run method into smaller methods which it
    will call. Each method will be called if the supplied attribute exists
    and is not empty. With smaller methods for each given action, it helps
    make the code easier to maintain and easier to write test cases.
    
    Relates to CID-3546.
    
    Change-Id: Ie6eee91926242f4fd9a4e03af90031f405d4c4dd

--- carbon/executors/runner.py
+++ carbon/executors/runner.py
@@ -68,6 +68,11 @@ class RunnerExecutor(CarbonExecutor):
         self.artifacts = getattr(package, 'artifacts')
         self.options = getattr(package, 'ansible_options', None)
 
+        # set ansible attributes
+        self.__set_ansible_attr__()
+
+    def __set_ansible_attr__(self):
+        """Set commonly used class attributes for ansible."""
         # create inventory object for create/delete inventory file
         self.inv = Inventory(
             self.hosts,
@@ -75,92 +80,121 @@ class RunnerExecutor(CarbonExecutor):
             data_dir=self.config['DATA_FOLDER']
         )
 
+        self.ans_verbosity = get_ans_verbosity(self.logger, self.config)
+        self.ans_controller = AnsibleController(self.inv.inventory_dir)
+        self.ans_extra_vars = dict(hosts=self.inv.group)
 
     def validate(self):
         """Validate."""
         raise NotImplementedError
 
+    @staticmethod
+    def build_ans_extra_args(attr, keys):
+        """Build ansible extra arguments for ansible ad hoc commands.
+
+        :param attr: key/values defined by task input
+        :type attr: dict
+        :param keys: keys used to build extra args
+        :type keys: list
+        :return: extra args
+        :rtype: str
+        """
+        extra_args = ''
+        for key in attr:
+            if key in keys:
+                extra_args += '%s=%s ' % (key, attr[key])
+        return extra_args
+
+    def __git__(self):
+        """Clone git repositories."""
+        # TODO: move these into constants
+        keys = ["repo", "version", "dest"]
+
+        extra_args = self.build_ans_extra_args(self.git, keys)
+
+        results = self.ans_controller.run_module(
+            "git",
+            logger=self.logger,
+            extra_vars=self.ans_extra_vars,
+            # run_options=run_options,
+            extra_args=extra_args,
+            ans_verbosity=self.ans_verbosity
+        )
+
+        # TODO: what to do with cloning results - fail on error
+        self.logger.info(results)
+
+    def __shell__(self):
+        """Execute the shell command."""
+        self.logger.info("running shell cmds here")
+
+        # execute the shell cmds one at a time
+        for shell in self.shell:
+            self.logger.info("Running shell cmd")
+            # create an ansible controller object
+
+            extra_args = self.build_ans_extra_args(self.shell, ['chdir'])
+
+            results = self.ans_controller.run_module(
+                "shell",
+                logger=self.logger,
+                extra_vars=self.ans_extra_vars,
+                # run_options=run_options,
+                script=shell["command"],
+                extra_args=extra_args,
+                ans_verbosity=self.ans_verbosity
+            )
+
+            # TODO: what to do with shell results - fail on error??
+            self.logger.info(results)
+
+    def __script__(self):
+        """Execute the script supplied."""
+        raise NotImplementedError
+
+    def __playbook__(self):
+        """Execute the playbook supplied."""
+        raise NotImplementedError
+
+    def __artifacts__(self):
+        """Archive artifacts produced by the tests."""
+        results = None
+        dest = os.path.join(self.datadir, 'artifacts')
+        self.logger.debug("Retrieve results here")
+        self.logger.debug("Storing results in %s" % dest)
+
+        for src_artifact in self.artifacts:
+            extra_args = "src=%s dest=%s mode=pull" % (src_artifact, dest)
+
+            results = self.ans_controller.run_module(
+                "synchronize",
+                logger=self.logger,
+                extra_vars=self.ans_extra_vars,
+                # run_options=run_options,
+                extra_args=extra_args,
+                ans_verbosity=self.ans_verbosity
+            )
+
+        # TODO: what to do with archive results - fail on error
+        self.logger.info(results)
+
     def run(self):
-        """Run."""
+        """Run.
+
+        The run method is the main entry point for the runner executor. This
+        method will invoke various other methods in order to successfully
+        run the runners execute types given.
+        """
 
         # create inventory if it doesn't exist
         if not os.path.exists(self.inv.master_inventory):
             self.inv.create()
 
-        ans_verbosity = get_ans_verbosity(self.logger, self.config)
-        # create an ansible controller object
-        obj = AnsibleController(self.inv.inventory_dir)
-        extra_vars = dict(hosts=self.inv.group)
+        # TODO: dynamically get the attribute names
+        for attr in ['git', 'shell', 'playbook', 'script', 'artifacts']:
+            # skip if the execute resource does not have the attribute defined
+            if not getattr(self, attr):
+                continue
 
-        # clone git
-        if self.git:
-            self.logger.debug("Cloning git here")
-
-            extra_args = ""
-            for key in self.git:
-                if key in ["repo", "version", "dest"]:
-                    extra_args += "%s=%s " % (key, self.git[key])
-
-            results = obj.run_module(
-                "git",
-                logger=self.logger,
-                extra_vars=extra_vars,
-                # run_options=run_options,
-                extra_args=extra_args,
-                ans_verbosity=ans_verbosity
-            )
-
-            # TODO: what to do with cloning results - fail on error
-            self.logger.info(results)
-
-        # execute user's shell command if that is set
-        if self.shell:
-            self.logger.info("running shell cmds here")
-            # execute the shell cmds one at a time
-            for shell in self.shell:
-                self.logger.info("Running shell cmd")
-                # create an ansible controller object
-
-                extra_args = ""
-                for key in shell:
-                  if key in ["chdir"]:
-                      extra_args += "%s=%s " % (key, shell[key])
-
-                results = obj.run_module(
-                    "shell",
-                    logger=self.logger,
-                    extra_vars=extra_vars,
-                    # run_options=run_options,
-                    script=shell["command"],
-                    extra_args=extra_args,
-                    ans_verbosity=ans_verbosity
-                )
-
-                # TODO: what to do with shell results - fail on error??
-                self.logger.info(results)
-
-
-        # TODO: add code to execute user's playbook if that is set
-
-        # TODO: add code to execute user's shell script if that is set
-
-        # TODO: retrieve artifacts (fetch ansible module)
-        if self.artifacts:
-            dest = os.path.join(self.datadir, 'artifacts')
-            self.logger.debug("Retrieve results here")
-            self.logger.debug("Storing results in %s" % dest)
-
-            for src_artifact in self.artifacts:
-                extra_args = "src=%s dest=%s mode=pull" % (src_artifact,dest)
-
-                results = obj.run_module(
-                    "synchronize",
-                    logger=self.logger,
-                    extra_vars=extra_vars,
-                    # run_options=run_options,
-                    extra_args=extra_args,
-                    ans_verbosity=ans_verbosity
-                )
-
-            # TODO: what to do with cloning results - fail on error
-            self.logger.info(results)
+            # call the method associated to the execute resource attribute
+            getattr(self, '__%s__' % attr)()
