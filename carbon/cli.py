d53531532792bc7c2055c89149d0b7463dc5f3ca
Danny Baez <dbaez@redhat.com>
2019-05-31 13:15:44 -0400

    CID-4250 Setting log_level to debug in carbon.cfg results in log level being info at runtime
     * Made changes to CLI to make the default param to None, so that what is in the carbon.cfg can be used
       if no --log-level option is used.
    
    closes: CID-4250
    
    Change-Id: I1e2fe040796d246583c326867b37c2d71c89ea95

--- carbon/cli.py
+++ carbon/cli.py
@@ -73,7 +73,7 @@ def create():
               help="Scenario workspace.")
 @click.option("--log-level",
               type=click.Choice(TASK_LOGLEVEL_CHOICES),
-              default='info',
+              default=None,
               help="Select logging level. (default=info)")
 @click.pass_context
 def validate(ctx, scenario, data_folder, log_level, workspace):
@@ -124,7 +124,7 @@ def validate(ctx, scenario, data_folder, log_level, workspace):
               help="Scenario workspace.")
 @click.option("--log-level",
               type=click.Choice(TASK_LOGLEVEL_CHOICES),
-              default='info',
+              default=None,
               help="Select logging level. (default=info)")
 @click.pass_context
 def run(ctx, task, scenario, log_level, data_folder, workspace):
