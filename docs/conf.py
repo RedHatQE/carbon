fd5f4ba682419229e7c2fa019c7e71ffc8c8b55e
Rujuta Shinde <rushinde@redhat.com>
2020-03-24 19:45:40 +0530

    Bump version: 1.5.0 → 1.5.1
    Change-Id: I34da0745a2a7455bc6e2c3daa24c0467cb08dd0c

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2018, Red Hat Inc.'
 author = u'Red Hat Inc.'
 
 # The short X.Y version
-version = u'1.5.0'
+version = u'1.5.1'
 # The full version, including alpha/beta/rc tags
-release = u'1.5.0'
+release = u'1.5.1'
 
 
 # -- General configuration ---------------------------------------------------
