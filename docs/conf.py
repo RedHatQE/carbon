0305be64a24604a8a3139775dbd122fe1b6e3563
Rujuta Shinde <rushinde@redhat.com>
2020-06-16 00:24:09 +0530

    Bump version: 1.6.0 → 1.7.0
    Change-Id: Idc997bc8fb94ec0d996344cebaa237e591691d57

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2018, Red Hat Inc.'
 author = u'Red Hat Inc.'
 
 # The short X.Y version
-version = u'1.6.0'
+version = u'1.7.0'
 # The full version, including alpha/beta/rc tags
-release = u'1.6.0'
+release = u'1.7.0'
 
 
 # -- General configuration ---------------------------------------------------
